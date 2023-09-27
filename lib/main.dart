0583f4bc577a2ac2cb88fbca87e5179ac9524f6c
berryxchange.innovations@gmail.com <berryxchange.innovations@gmail.com>
2023-09-26 21:48:56 -0500

    Added Future basics to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_portfolio_project/StateManagement/InheritedWidget/plan_provider.dart';
-import 'package:flutter_portfolio_project/StateManagement/Model-View/View/plan_screen.dart';
-import 'package:flutter_portfolio_project/StateManagement/Multi-Screen/Views/plan_creator_screen.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(PlanProvider(
-        key: null,
-        child: MasterPlanApp()));
+  runApp(MyApp());
 }
 
-class MasterPlanApp extends StatelessWidget {
-  const MasterPlanApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: PlanCreatorScreen(),
+      title: "Future Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FuturePage(),
     );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  late String result = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Back from the Future"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Spacer(),
+            ElevatedButton(
+                onPressed: () {
+                  result = "";
+                  setState(() {
+                    result = result;
+                  });
+                  getData().then((value) {
+                    result = value.body.toString().substring(0, 450);
+                    setState(() {
+                      result = result;
+                    });
+                  }).catchError((_) {
+                    result = "An error occurred";
+                    setState(() {
+                      result = result;
+                    });
+                  });
+                },
+                child: Text("Go!")),
+            Spacer(),
+            Text(result.toString()),
+            Spacer(),
+            CircularProgressIndicator(),
+            Spacer()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<http.Response> getData() async {
+    final String authority = "www.googleapis.com";
+    final String path = "books/v1/volumes/junbDwAAQBAJ";
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
   }
 }
